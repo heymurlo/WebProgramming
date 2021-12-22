a60ed3c1105052576c14fe5b6108c9c908d3be66
heymurlo <sultangirova_d@mail.ru>
2021-12-22 16:14:42 +0300

    error processing fixed, arrow style changed

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,8 +1,11 @@
 window.onload = function () {
     let elem = document.querySelector('.main-carousel');
+    let btn = document.querySelector('.flickity-button')
     let flkty = new Flickity(elem, {
         prevNextButtons: true, //стрелки для перелистывания
         wrapAround: true, //бесконечный скроллинг
-        groupCells: 3 //кол-во отображаемых картинок в актуальной строке
+        groupCells: 3, //кол-во отображаемых картинок в актуальной строке
+
     });
 }
+
