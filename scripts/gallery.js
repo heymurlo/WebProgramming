bdaf2e3d1aa71781cd81ff0add893b09484c80a9
heymurlo <sultangirova_d@mail.ru>
2021-12-22 11:59:54 +0300

    added 6 7

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -3,6 +3,6 @@ window.onload = function () {
     let flkty = new Flickity(elem, {
         prevNextButtons: true, //стрелки для перелистывания
         wrapAround: true, //бесконечный скроллинг
-        groupCells: 3 //кол-во отображаемых элементов
+        groupCells: 3 //кол-во отображаемых картинок в актуальной строке
     });
 }
